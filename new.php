daaa39ed393141a8365643db04fe75c9e3b393dd
nanita1215 <ana.jurado2@misena.edu.co>
2022-08-16 11:44:15 -0500

    ok

---
+++ new.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>
+  <title>Problema</title>
+</head>
+
+<body>
+  <?php
+  require_once("new2.php");
+  cabeceraPagina("Titulo principal de la página");
+  echo "<br><br><center>Este es el cuerpo de la página<br><br></center>";
+  piePagina("Pie de la página");
+  ?>
+</body>
+
+</html>
